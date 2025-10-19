ebd1c030b84c81bf3257362cf0d17bd22e21668b
Saul-Arnica <saulagustinarnica@outlook.com>
2025-10-19 15:44:54 -0300

    Terminado el punto 6 y 7

---
+++ Practico 1/Practico_1/Punto_6/Ejercicio_6.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * 6. Clasificacion por edad
+ * Pedir la edad  mostrar si la persona es niño, adolescente, adulto o adulto mayor("viejo").
+ */
+
+using System;
+
+namespace Punto_6;
+
+public class Ejercicio_6
+{
+    public static void Main(/*string[] args*/)
+    {
+        Console.Write("Ingrese su edad:");
+        string? ingreso = Console.ReadLine();
+
+        if (!int.TryParse(ingreso, out int edad))
+        {
+            Console.WriteLine("Lo ingresado no es un numero o caracter valido. Intente de nuevo.");
+        }
+        else
+        {
+            if(edad >= 0 && edad <= 12)
+            {
+                Console.WriteLine("\nEs niño");
+
+            }else if(edad >= 13 && edad <= 17)
+            {
+                Console.WriteLine("\nEs adolescente");
+
+            }else if(edad >= 18 && edad <= 60)
+            {
+                Console.WriteLine("\nEs adulto");
+
+            }else if(edad > 60)
+            {
+                Console.WriteLine("\nTas mas cerca del cielo que de la tierra campeon!");
+            }
+        }
+
+    }
+}
---
+++ Practico 1/Practico_1/Punto_7/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * 7. Numero dentro de rango.
+ * Pedir un numero y verificar si está dentro del rango 1-100 (inclusive)
+ */
+
+using System;
+
+namespace Punto_7;
+
+public class Ejercicio_7
+{
+    public static void Main(/*string[] args*/)
+    {
+        Console.Write("Ingrese un numero dentro del rango del 1 al 100:");
+        string? ingreso = Console.ReadLine();
+
+        if(!int.TryParse(ingreso, out int NumDeIngreso))
+        {
+            Console.WriteLine("\nColocaste cualquiera chamigo, pone un numero dentro del 1 al 100...");
+        }
+        else
+        {
+            if(NumDeIngreso > 0 && NumDeIngreso <= 100)
+            {
+                Console.WriteLine("\nEsta dentro del rago, bien ahi loco");
+            }
+            else if(NumDeIngreso > 100)
+            {
+
+                Console.WriteLine("\nComo te dicen? Mbapé? te fuiste re lejos que le pegaste al traba en la tribuna");
+
+            }else if(NumDeIngreso < 0)
+            {
+
+                Console.WriteLine("\nJiaaa no sabe leer me parece che...");
+            }
+        }
+    }
+}
