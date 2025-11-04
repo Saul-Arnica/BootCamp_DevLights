06037bc7466091b74ca5406269ee82d2e7ea7051
Saul-Arnica <saulagustinarnica@outlook.com>
2025-11-04 17:56:03 -0300

    Terminando el 6 :S

--- Practico 2/Punto_6/Ejercicio_6.cs
+++ Practico 2/Punto_6/Ejercicio_6.cs
@@ -20,6 +20,54 @@ public class Ejercicio_6
 {
     public static void Main(/*string[] args*/)
     {
+        int[,] temp = new int[5, 7];
+        string[] dias = { "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb", "Dom" };
+        Random rand = new();
 
+        // Rellenar con valores entre 7 y 38
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 7; j++)
+                temp[i, j] = rand.Next(7, 39);
+        }
+
+        int tempMaxMes = int.MinValue;
+        string diaMaxMes = "";
+        int semanaMaxMes = 0;
+
+        // Procesar semanas
+        for (int i = 0; i < 5; i++)
+        {
+            int suma = 0;
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            int diaMax = 0;
+            int diaMin = 0;
+
+            for (int j = 0; j < 7; j++)
+            {
+                int t = temp[i, j];
+                suma += t;
+
+                if (t > max) { max = t; diaMax = j; }
+                if (t < min) { min = t; diaMin = j; }
+
+                if (t > tempMaxMes)
+                {
+                    tempMaxMes = t;
+                    diaMaxMes = dias[j];
+                    semanaMaxMes = i + 1;
+                }
+            }
+
+            double promedio = suma / 7.0;
+            Console.WriteLine($"\nSemana {i + 1}:");
+            Console.WriteLine($"-Máxima: {max}° ({dias[diaMax]})");
+            Console.WriteLine($"-Mínima: {min}° ({dias[diaMin]})");
+            Console.WriteLine($"-Promedio semanal: {promedio:F2}°");
+        }
+
+        Console.WriteLine($"\nTemperatura más alta del mes: {tempMaxMes}° en la semana " +
+                                                                                $"{semanaMaxMes} ({diaMaxMes})");
     }
 }
