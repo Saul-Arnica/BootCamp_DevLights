96f6dcc5661eb80e0a9ce8b7e96efb642fbbaebb
Saul-Arnica <saulagustinarnica@outlook.com>
2025-11-04 16:52:19 -0300

    Haciendo el punto 1 y 2 del segundo practico

---
+++ Practico 2/Punto_1/Ejercicio_1.cs
@@ -0,0 +1,47 @@
+﻿/*
+ *  1. Dado que se tiene almacenado en una lista, los resultados de los últimos 10 
+ *  exámenes de un alumno, calcular su promedio y mostrar por pantalla las 10 notas de
+ *  los exámenes y el promedio resultante
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Punto_1;
+
+public class Ejercicio_1
+{
+    public static void Main(/*string[] args*/)
+    {
+        double promedio = 0;
+        int contador = 1;
+        List<int> notas =
+        //Agregamos las notas.
+        [
+            6,
+            7,
+            7,
+            8,
+            7,
+            7,
+            7,
+            6,
+            8,
+            9,
+        ]; 
+        //Tambien puede ser "var notas = new List<int>();" y luego notas.Add(x);
+        
+        Console.WriteLine("Las notas del alumno fueron:");
+        foreach (int i in notas)
+        {
+            
+            promedio += i;
+            Console.WriteLine($"Nota {contador}:{i}");
+            contador++;
+        }
+
+        promedio /= notas.Count; //esto es lo mismo que promedio = promedio / notas.Count;
+        Console.WriteLine($"El promedio del alumno es: {promedio:F2}");
+    }
+}
---
+++ Practico 2/Punto_2/Ejercicio_2.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * 2. Dada las edades de 20 personas guardadas en una lista, imprimir por pantalla
+ * cuántos son mayores de edad y cuántos no. 
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Punto_2;
+
+public class Ejercicio_2
+{
+    public static void Main(/*string[] args*/)
+    {
+        // Lista de edades de 20 personas (pueden ser fijas o aleatorias)
+        List<int> edades = new List<int>();
+        Random rand = new Random();
+
+        for (int i = 0; i < 20; i++)
+        {
+            edades.Add(rand.Next(10, 60)); // edades entre 10 y 60
+        }
+
+        int mayores = 0;
+        int menores = 0;
+
+        Console.WriteLine("Edades registradas:\n");
+
+        foreach (int edad in edades)
+        {
+            Console.Write(edad + " ");
+
+            if (edad >= 18)
+                mayores++;
+            else
+                menores++;
+        }
+
+        Console.WriteLine("\n---------------------------");
+        Console.WriteLine($"Total de personas: {edades.Count}");
+        Console.WriteLine($"Mayores de edad: {mayores}");
+        Console.WriteLine($"Menores de edad: {menores}");
+    }
+}
